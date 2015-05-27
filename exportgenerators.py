914fe43d2b2caa7f46be75f469090e9f4bd295e0
Sheehan Olver <solver@mac.com>
2015-05-27 11:04:57 +1000

    added generator export

---
+++ exportgenerators.py
@@ -0,0 +1,11 @@
+k=1;s=range(1,n+1);s[k-1],s[k]=s[k],s[k-1];np.savetxt('/Users/solver/Desktop/'+str(k)+'.csv',spc(s),delimiter=",")
+
+
+
+l=[3,2]
+spc=SymmetricGroupRepresentation(l,"specht");n=sum(l);
+for k in range(1,n):
+    s=range(1,n+1);s[k-1],s[k]=s[k],s[k-1]
+    gen=spc(s)
+    np.savetxt('/tmp/'+str(k)+'.csv',gen,delimiter=",")
+    
