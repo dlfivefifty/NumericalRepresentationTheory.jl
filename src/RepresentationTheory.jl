e7e1ad7f3154d032590fea208dc5efaf74107fb8
Sheehan Olver <dlfivefifty@gmail.com>
2018-02-26 12:35:57 +0000

    Use symmetry, simplify things

--- src/RepresentationTheory.jl
+++ src/RepresentationTheory.jl
@@ -1,12 +1,14 @@
 module RepresentationTheory
 using Base, Compose, Compat
 import Base: ctranspose, transpose, getindex, size, setindex!, maximum, Int, length,
-                ==, isless, copy
+                ==, isless, copy, kron, eig, hash, first
 ## Kronecker product of Sn
 
+
+
 export perm, permkronpow, permmults, topart, plotmults,
-    irrepgenerators, standardgenerators, Partition, YoungMatrix, partitions,
-    youngtableaux, YoungTableau
+    standardgenerators, Partition, YoungMatrix, partitions,
+    youngtableaux, YoungTableau, ⊗, Representation, multiplicities
 
 function perm(a,b,n)
     ret = eye(n)
@@ -29,136 +31,9 @@ end
 permkronpow(m) = (a,b,n)->kronpow(perm(a,b,n),m)
 
 ## Algorithm
-
-function wilkshift(A)
-    if size(A,1)==1
-        A[1,1]
-    else
-        a1,a,b=A[end-1,end-1],A[end,end],A[end-1,end]
-        if a1 == a == b == 0
-            0.0
-        else
-            δ=(a1-a)/2
-            a-sign(δ)*b^2/(abs(δ)+sqrt(δ^2+b^2))
-        end
-    end
-end
-
-
-function slnorm(m,kr,jr)
-    ret=0.0
-    for j=jr
-        for k=kr
-            @inbounds ret=ret+abs2(m[k,j])
-        end
-    end
-    ret
-end
-
-function deflateblock(v)
-    tol=10E-8
-    for m=1:size(v[1],1)-1
-        allzero=true
-        for vk in v
-            if slnorm(vk,m+1:size(vk,2),1:m) >tol
-                allzero=false
-                break
-            end
-        end
-
-        if allzero
-            w1 = Vector{Matrix{Complex128}}(length(v))
-            w2 = Vector{Matrix{Complex128}}(length(v))
-            for k = 1:length(v)
-                w1[k] = v[k][1:m,1:m]
-                w2[k] = v[k][m+1:end,m+1:end]
-            end
-            return w1,w2
-        end
-    end
-
-    # no subblocks
-    v,Vector{Matrix{Complex128}}(0)
-end
-
-function deflate(v)
-    if size(v[1],1)==1
-        return v
-    end
-
-    ret=Vector{Vector{Matrix{Complex128}}}(size(v[1],1))
-
-    for n=1:size(v[1],1)
-        w1,v=deflateblock(v)
-        ret[n]=w1
-        if isempty(v)
-            return ret[1:n]
-        end
-    end
-
-    error("There's a bug: Should have less blocks then dimension of matrix")
-end
-
-
-function nullQ(w,μ)
-#    @assert imag(μ)<1000000eps()
-    svd(w-μ*I)[end][:,end:-1:1]
-end
-nullQ(M)=nullQ(M,eigvals(M)|>first)
-
-
-# returns Q
-function simdiagonalize{T}(v::Vector{Matrix{T}})
-    if size(v[1],1)==1
-        return eye(1)
-    end
-
-    for k=1:length(v)
-        if slnorm(v[k],2:size(v[k],1),1)>100000eps()
-            Q=nullQ(v[k])
-            df=deflate(map(v->Q'*v*Q,v))
-            if length(df)>1
-                Q2=simdiagonalize(df)
-                return Q*Q2
-            end
-        end
-    end
-
-    ## Already diagonalized
-    warn("Matrix not deflated.  Returning current decomposition.")
-    eye(size(v[1],1))
-end
-
-# a list of each blocks
-# returns Q
-function simdiagonalize{T}(v::Vector{Vector{Matrix{T}}})
-    d=mapreduce(vk->size(vk[1],1),+,v)
-    Q=zeros(Complex{Float64},d,d)
-    m=1
-    for j= 1:length(v)
-        Qk=simdiagonalize(v[j])
-        dk=size(Qk,1)
-        Q[m:m+dk-1,m:m+dk-1]=Qk
-        m+=dk
-    end
-    Q
-end
-
-function gelfandbasis(perm,n)
-    w=Array(Array{Float64,2},n-1)
-
-    for k=1:n-1
-        w[k]=perm(1,k+1,n)
-        for j=2:k
-            w[k]+=perm(j,k+1,n)
-        end
-    end
-    w
-end
-
-function gelfandbasis(gen)
+function gelfandbasis(gen::Vector{MT}) where MT
     n=length(gen)+1
-    w=Vector{Matrix{Float64}}(n-1)
+    w=Vector{MT}(n-1)
     for k=1:n-1
         a=gen[k]
         w[k]=a
@@ -169,119 +44,6 @@ function gelfandbasis(gen)
     end
     w
 end
-
-
-function reducesn(perm...)
-    w=gelfandbasis(perm...)
-    simdiagonalize(deflate(w)),w
-end
-
-
-
-
-
-function permmults(perm...)
-    Q,v=reducesn(perm...)
-    map(x->round(Int,real(x)),hcat(map(v->diag(Q'*v*Q),v)...))
-end
-
-
-
-
-function partmults(parts)
-    dict=Dict{Vector{Int64},Int64}()
-
-    for part in parts
-        if !haskey(dict,part)
-            dict[part]=0
-        end
-        dict[part]+=1
-    end
-    dict
-end
-
-function topart(part::Vector)
-    part=sort(part)
-    p=zeros(Int64,1-first(part))
-    k=1
-    while k≤length(part)
-        pk=part[k]
-        rk=pk<0?1-pk:1
-        p[rk]+=1
-        k+=1
-        while k≤length(part)&&part[k]==pk
-            k+=1
-            rk+=1
-            p[rk]+=1
-        end
-    end
-    p
-end
-
-function topart(m::Matrix)
-    ret=Vector{Vector{Int64}}(size(m,1))
-    for k=1:size(m,1)
-        ret[k]=topart([0;vec(m[k,:])])
-    end
-    ret
-end
-
-
-
-## Plotting
-
-function plotpart(part)
-    ε=0.01
-    x,y=part[1],length(part)
-    box(ε,k,j,x,y)=rectangle((k-1)/x+ε,(j-1)/y+ε,1/x-ε,1/y-ε)
-    cnt=compose(context())
-
-    for k=1:length(part),j=1:part[k]
-        cnt=compose(cnt,box(ε,j,k,x,y))
-    end
-
-    compose(cnt,fill("tomato"),stroke("black"))
-end
-
-
-
-function plotmults(dict::Dict)
-    cnt=compose(context());k=0
-    kys=keys(dict)
-    ml=mapreduce(length,max,kys)
-    nl=mapreduce(first,max,kys)
-    ε=0.05
-    m=ceil(Integer,sqrt(length(kys)))
-    for ky in kys
-        j,i=div(k,m),mod(k,m)
-        compose!(cnt,(context(i/m+ε/m,j/m,first(ky)/(nl*m)-ε/m,0.5*length(ky)/(nl*ml)),RepresentationTheory.plotpart(ky)),
-                 (context(),text(i/m*(1+ε)+0.2/m,j/m+(0.6+1/ml)/nl,string(dict[ky])),fontsize(4)))
-        k+=1
-    end
-    cnt
-end
-
-
-function plotmults(perm...)
-    mults=permmults(perm...)
-    plotmults(partmults(topart(mults)))
-end
-
-
-
-# function irrepgenerators(part)
-#     run(`/Applications/SageMath-8.1.app/sage $(Pkg.dir())/RepresentationTheory/exportgenerators.py $part`)
-#     n=sum(part)
-#     ret=Array{Matrix{Float64}}(n-1)
-#     for k=1:n-1
-#        ret[k]=readcsv("/tmp/gen"*string(k)*".csv")
-#     end
-#     ret
-# end
-
-
-
-
 
 struct Partition
     σ::Vector{Int}
@@ -305,7 +67,8 @@ function isless(a::Partition, b::Partition)
 
     return false
 end
-Base.:(==)(a::Partition, b::Partition) = a.σ == b.σ
+(==)(a::Partition, b::Partition) = a.σ == b.σ
+hash(a::Partition) = hash(a.σ)
 
 copy(a::Partition) = Partition(copy(a.σ))
 
@@ -323,7 +86,7 @@ end
 ctranspose(σ::Partition) = transpose(σ)
 getindex(σ::Partition, k::Int) = σ.σ[k]
 setindex!(σ::Partition, v, k::Int) = setindex!(σ.σ, v, k)
-for op in (:maximum, :length)
+for op in (:maximum, :length, :first)
     @eval $op(σ::Partition) = $op(σ.σ)
 end
 
@@ -490,5 +253,147 @@ end
 
 irrepgenerators(σ::Partition) = [irrepgenerator(σ, i) for i=1:Int(σ)-1]
 
+
+
+struct Representation{MT}
+       generators::Vector{MT}
+end
+
+Representation(σ::Partition) = Representation(irrepgenerators(σ))
+kron(A::Representation, B::Representation) = Representation(kron.(A.generators, B.generators))
+⊗(A::Representation, B::Representation) = kron(A, B)
+
+
+# determine multiplicities of eigs on diagonal, assuming sorted
+function eigmults(λ::Vector{Int})
+       mults = Vector{Int}(uninitialized)
+       n = length(λ)
+       tol = 0.01 # integer coefficents
+       c = 1
+       for k = 2:n
+           if λ[k] > λ[k-1]
+              push!(mults, c)
+              c = 0
+           end
+           c += 1
+       end
+       push!(mults, c)
+       mults
+end
+
+gelfand_reduce(R::Representation) = gelfand_reduce(Matrix.(gelfandbasis(R.generators)))
+
+function gelfand_reduce(X)
+       λ̃, Q₁ = eig(Symmetric(X[1]))
+       λ = round.(Int, λ̃)
+       length(X) == 1 && return reshape(λ,length(λ),1),Q₁
+
+       m = eigmults(λ)
+       c_m = [0;cumsum(m)]
+
+       Q = zeros(Q₁)
+       Λ = Matrix{Int}(uninitialized, length(λ), length(X))
+       Λ[:,1] = λ
+
+       for j=2:length(c_m)
+              j_inds = c_m[j-1]+1:c_m[j]
+              Qʲ = Q₁[:,j_inds] # deflated rows
+              Xⱼ = map(X -> ctranspose(Qʲ)*X*Qʲ, X[2:end])
+              Λⱼ, Qⱼ = gelfand_reduce(Xⱼ)
+              Q[j_inds,j_inds] = Qⱼ
+              Λ[j_inds,2:end] = Λⱼ
+       end
+
+       Λ, Q₁*Q
+end
+
+function topart(part::Vector)
+    part=sort(part)
+    p=zeros(Int64,1-first(part))
+    k=1
+    while k≤length(part)
+        pk=part[k]
+        rk=pk<0?1-pk:1
+        p[rk]+=1
+        k+=1
+        while k≤length(part)&&part[k]==pk
+            k+=1
+            rk+=1
+            p[rk]+=1
+        end
+    end
+    Partition(p)
+end
+
+function topart(m::Matrix{Int})
+    ret = Vector{Partition}(size(m,1))
+    for k=1:size(m,1)
+        ret[k] = topart([0;vec(m[k,:])])
+    end
+    ret
+end
+
+# these are the multiplicities without dividing by dimension
+function _multiplicities(parts::Vector{Partition})
+    dict = Dict{Partition,Int64}()
+
+    for part in parts
+        if !haskey(dict,part)
+            dict[part] = 0
+        end
+        dict[part] += 1
+    end
+    dict
+end
+
+function multiplicities(Λ::Vector{Partition})
+    mults = _multiplicities(Λ)
+
+    for σ in keys(mults)
+           mults[σ] = mults[σ] ÷ hooklength(σ)
+    end
+
+    mults
+end
+
+multiplicities(R::Representation) = multiplicities(topart(gelfand_reduce(R)[1]))
+
+
+
+## Plotting
+
+function plotpart(part::Partition)
+    ε=0.01
+    x,y=part[1],length(part)
+    box(ε,k,j,x,y)=rectangle((k-1)/x+ε,(j-1)/y+ε,1/x-ε,1/y-ε)
+    cnt=compose(context())
+
+    for k=1:length(part),j=1:part[k]
+        cnt=compose(cnt,box(ε,j,k,x,y))
+    end
+
+    compose(cnt,fill("tomato"),stroke("black"))
+end
+
+
+
+function plotmults(dict::Dict)
+    cnt = compose(context());k=0
+    kys = keys(dict)
+    ml = mapreduce(length,max,kys)
+    nl = mapreduce(first,max,kys)
+    ε=0.05
+    m=ceil(Integer,sqrt(length(kys)))
+    for ky in kys
+        j,i=div(k,m),mod(k,m)
+        compose!(cnt,(context(i/m+ε/m,j/m,first(ky)/(nl*m)-ε/m,0.5*length(ky)/(nl*ml)),RepresentationTheory.plotpart(ky)),
+                 (context(),text(i/m*(1+ε)+0.2/m,j/m+(0.6+1/ml)/nl,string(dict[ky])),fontsize(4)))
+        k+=1
+    end
+    cnt
+end
+
+
+plotmults(R::Representation) = plotmults(multiplicities(R))
 
 end #module
