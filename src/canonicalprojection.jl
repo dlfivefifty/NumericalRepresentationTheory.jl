b1ad0ba00dd2c05f765712110e669a4d348f7274
Sheehan Olver <solver@mac.com>
2024-06-12 07:47:36 +0100

    Implement canonical representation (#6)
    * Implement canonical representation
    
    * multiplicities
    
    * Update runtests.jl
    
    * update tests
    
    * Update runtests.jl

--- src/canonicalprojection.jl
+++ src/canonicalprojection.jl
@@ -1,3 +1,10 @@
 ###############
 # This implements the "canonical projection" a la Hymabaccus 2020
 ##############
+
+function canonicalprojection(σ::Partition, ρ)
+    n = Int(σ)
+    ρ_σ = Representation(σ)
+    n_σ = size(ρ_σ,1)
+    n_σ/factorial(n) * sum(tr(ρ_σ(g))ρ(g) for g in PermGen(n))
+end 
